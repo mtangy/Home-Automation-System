9401d76fcf38c6485e6e9d68ddf350d88b9187f3
Michael Tangy <mike.tangy.91@gmail.com>
2016-09-24 20:23:33 -0500

    Update homeInter.php

--- homeInter.php
+++ homeInter.php
@@ -4,25 +4,25 @@
 		<H1>Air Conditioning Power(Relay 1)</H1>
 		<H2>Turn AC On/Off</H2>
 		<H3>
-			<p>	
-			    <form action="relayToggle.php" method="get">
+		     <p>	
+		<form action="relayToggle.php" method="get">
                 <input type="submit" value="AC Power">
                 </form>
  			</p>
 			<form method= "post" action = "homeInter.php">
 			<p>
 			    <label for="acTemp">AC temperature (F):</label><br/>
-                <input type="text" id="temp" name="temp">
+                            <input type="text" id="temp" name="temp">
 			</p>
-            <button type="submit" name="submit" value="send">Change Temperature</button>
-            </form>
+                 <button type="submit" name="submit" value="send">Change Temperature</button>
+                 </form>
 			<p><strong>
 			    <?php 			        
-					$temp= $_POST['temp'];
-                    $tempFile = fopen('/usr/local/bin/temp.txt','w');
-					fwrite($tempFile,$temp);
-                    fclose($tempFile);	
-				?>
+			       $temp= $_POST['temp'];
+                               $tempFile = fopen('/usr/local/bin/temp.txt','w');
+			       fwrite($tempFile,$temp);
+                               fclose($tempFile);	
+			    ?>
 			</strong>
 			</p>
 		</H3>
