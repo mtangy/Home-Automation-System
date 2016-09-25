71c7631af20b4d9afcd0d1c690761eff7a3cf035
Michael Tangy <mike.tangy.91@gmail.com>
2016-09-25 17:30:12 -0500

    Update relayToggle.py

--- relayToggle.py
+++ relayToggle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
-#import time
-import Adafruit_BBIO.GPIO as GPIO
-#import socket # Import socket module
+#This script is called by relayToggle.php to toggle/flip the logic pin that closes the relay 
+#that complete's the circuit that powers the AC
 
 
-pin = "P8_14"
-GPIO.cleanup()
-GPIO.setup(pin, GPIO.OUT)
+import Adafruit_BBIO.GPIO as GPIO            # import BBB GPIO python library 
 
-f = open('/usr/local/bin/relayState.txt', 'r+')
-relayState = f.read().strip()
-f.seek(0)
+pin = "P8_14"                                #assign which pin is used to activate the relay
+GPIO.cleanup()                               #reset GPIO pins before changing 
+GPIO.setup(pin, GPIO.OUT)                    #set pin as output
 
-if relayState == 'off':
+f = open('/usr/local/bin/relayState.txt', 'r+')  #open file containg surrent state of relay
+relayState = f.read().strip()                    #read and parse and assign string to variable 
+f.seek(0)                                        # change file objects position to 0 inorder to completly rewrite file
+
+if relayState == 'off':                           
     GPIO.output(pin, GPIO.LOW)
     f.write("on ")
     print "AC ON"
