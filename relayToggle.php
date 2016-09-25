36c9af95fad0594decaa411191d1743dd9500e56
Michael Tangy <mike.tangy.91@gmail.com>
2016-09-25 17:07:46 -0500

    Update relayToggle.php

--- relayToggle.php
+++ relayToggle.php
@@ -1,7 +1,7 @@
 <p>	
     <?php 
-	$command = escapeshellcmd('sudo python /usr/local/bin/relayToggle.py'); 
-	$acState = shell_exec($command); 
-    echo $acState; 
+	$command = escapeshellcmd('sudo python /usr/local/bin/relayToggle.py'); //PHP to execute the python script that toggles relay
+	$acState = shell_exec($command);         //Output of script gets assigned to varable in orger to indicate if AC is on or off
+        echo $acState; 
     ?>
 </p>
