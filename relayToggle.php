f2bedaa120b3523beb59b2ee3ce5edbb5c0176af
Michael Tangy <mike.tangy.91@gmail.com>
2016-10-12 10:58:52 -0500

    Update relayToggle.php

--- relayToggle.php
+++ relayToggle.php
@@ -1,8 +1,8 @@
 <p>	
     <?php 
-	$command = escapeshellcmd('./usr/local/bin/kill.sh');   
-	$command = escapeshellcmd('sudo python /usr/local/bin/relayToggle.py'); 
-	$acState = shell_exec($command); 
+	$command = escapeshellcmd('./usr/local/bin/kill.sh');   <!--kill previous instance of relayToggle.php-->
+	$command = escapeshellcmd('sudo python /usr/local/bin/relayToggle.py'); <!--execute relayToggle.php-->
+	$acState = shell_exec($command); <!--display state of AC-->
     echo $acState; 
     ?>
 </p>
