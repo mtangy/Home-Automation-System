b234935d2a4eb906f3a923b81e72fb80d31df8db
Michael Tangy <mike.tangy.91@gmail.com>
2016-10-12 11:00:19 -0500

    Update relayToggle.py

--- relayToggle.py
+++ relayToggle.py
@@ -4,24 +4,20 @@
 #that complete's the circuit that powers the AC
 
 
-import Adafruit_BBIO.GPIO as GPIO            # import BBB GPIO python library 
+import Home_Automation
+import Adafruit_BBIO.GPIO as GPIO
 
-pin = "P8_14"                                #assign which pin is used to activate the relay
-GPIO.cleanup()                               #reset GPIO pins before changing 
-GPIO.setup(pin, GPIO.OUT)                    #set pin as output
+#GPIO.cleanup()
 
-f = open('/usr/local/bin/relayState.txt', 'r+')  #open file containg surrent state of relay
-relayState = f.read().strip()                    #read and parse and assign string to variable 
-f.seek(0)                                        # change file objects position to 0 inorder to completly rewrite file
-
-if relayState == 'off':                           
-    GPIO.output(pin, GPIO.LOW)
-    f.write("on ")
+if not Home_Automation.checkACon():
+    Home_Automation.turnACon()
     print "AC ON"
-	
-elif relayState == 'on':
-    GPIO.output(pin, GPIO.HIGH)
-    f.write("off")
+    while True:
+        pass
+        	    
+else:
+    Home_Automation.turnACoff()
     print "AC OFF"
-	
-f.close()
+    while True:
+        pass
+        
