9d21ff18a021c0da75e7b26a92d733fecb0fc2f0
Michael Tangy <mike.tangy.91@gmail.com>
2016-10-12 10:50:06 -0500

    Update serverHome.py

--- serverHome.py
+++ serverHome.py
@@ -1,4 +1,4 @@
-import Adafruit_BBIO.ADC as GPIO
+import Home_Automation
 import socket # Import socket module
 import time
 
@@ -9,37 +9,29 @@ s.listen(5)   # Now wait for client connection.
 
 f = open('serverLog.txt', 'w')
 
-pin = "P9_40"
-GPIO.setup(pin, GPIO.OUT)
-
 c, addr = s.accept()       # Establish connection with client.
-f.write(str(addr)+" - "time.strftime("%H:%M:%S")+"\n")
+addr = str(addr[0])
+f.write(str(addr)+" - "+time.strftime("%H:%M:%S")+"\n")
 
-if(addr[:7] == '192.168'):  #checks that client IP is local
-    while True:
-        packetContents = str(c.recv(1024))
+if(addr[0:7] == '192.168'):  #checks that client IP is local
+
+    while True:                               #constantly run process  
+        packetContents = str(c.recv(1024))    #wait for Rpi client to send packet indicating door changes state
+        print packet contents                 #print packet contents for debugging purposes 
 		
-		if packetContents == "door open":
-		     relayState = Rs.read().strip()
-			 if relayState == "on":
-                 GPIO.output(pin, GPIO.HIGH)
-				 Rs = open('/usr/local/bin/relayState.txt', 'r+')
-				 Rs.seek(0)
-				 Rs.write("off")
-				 Rs.close()
-		     else:
-			     continue
-		if packetContents == "door closed":
-		     relayState = Rs.read().strip()
-			 if relayState == "off":
-                 GPIO.output(pin, GPIO.LOW)
-				 Rs = open('/usr/local/bin/relayState.txt', 'r+')
-				 Rs.seek(0)				 
-				 Rs.write("on")
-				 Rs.close()
-		     else:
-			     continue		
-                 	
+        if packetContents == "door open" and Home_Automation.checkACon():  #Check if door is open and AC is on
+            Home_Automation.turnACoff()                               #rewrite state of file now that AC is off
+        else:
+            continue
+
+        if packetContents == "door closed" and not Home_Automation.checkACon(): 
+            Home_Automation.turnACon()
+        else:
+            continue
+
 else:
+
+    print "address not local"
+
     f.write("client from outside subnet\n")
     c.close() # Close the connection
